68dfec694f49f04d9e85ad2a46797fc5dc9690fa
AshleyPullen <85501589+ashleypullen@users.noreply.github.com>
2022-01-21 23:35:50 +0000

    initial commit

---
+++ Menus/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace Menus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewProj = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewFiles = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewTextBoxes = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewLabels = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewImages = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFD = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.toolStripMenuItem1,
+            this.mnuQuit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuViewProj,
+            this.mnuViewFiles});
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mnuNew.Size = new System.Drawing.Size(186, 22);
+            this.mnuNew.Text = "&New";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.mnuOpen.Size = new System.Drawing.Size(186, 22);
+            this.mnuOpen.Text = "&Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuSave.Size = new System.Drawing.Size(186, 22);
+            this.mnuSave.Text = "&Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(183, 6);
+            // 
+            // mnuQuit
+            // 
+            this.mnuQuit.Name = "mnuQuit";
+            this.mnuQuit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.mnuQuit.Size = new System.Drawing.Size(186, 22);
+            this.mnuQuit.Text = "&Quit";
+            this.mnuQuit.Click += new System.EventHandler(this.mnuQuit_Click);
+            // 
+            // mnuViewProj
+            // 
+            this.mnuViewProj.Name = "mnuViewProj";
+            this.mnuViewProj.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.mnuViewProj.Size = new System.Drawing.Size(180, 22);
+            this.mnuViewProj.Text = "View &Project";
+            // 
+            // mnuViewFiles
+            // 
+            this.mnuViewFiles.Name = "mnuViewFiles";
+            this.mnuViewFiles.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.mnuViewFiles.Size = new System.Drawing.Size(180, 22);
+            this.mnuViewFiles.Text = "View &Files";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuUndo,
+            this.mnuCut,
+            this.mnuCopy,
+            this.mnuPaste});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuViewTextBoxes,
+            this.mnuViewLabels,
+            this.mnuViewImages});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // mnuUndo
+            // 
+            this.mnuUndo.Name = "mnuUndo";
+            this.mnuUndo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.mnuUndo.Size = new System.Drawing.Size(180, 22);
+            this.mnuUndo.Text = "&Undo";
+            this.mnuUndo.Click += new System.EventHandler(this.mnuUndo_Click);
+            // 
+            // mnuCut
+            // 
+            this.mnuCut.Name = "mnuCut";
+            this.mnuCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.mnuCut.Size = new System.Drawing.Size(180, 22);
+            this.mnuCut.Text = "Cu&t";
+            this.mnuCut.Click += new System.EventHandler(this.mnuCut_Click);
+            // 
+            // mnuCopy
+            // 
+            this.mnuCopy.Name = "mnuCopy";
+            this.mnuCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.mnuCopy.Size = new System.Drawing.Size(180, 22);
+            this.mnuCopy.Text = "&Copy";
+            this.mnuCopy.Click += new System.EventHandler(this.mnuCopy_Click);
+            // 
+            // mnuPaste
+            // 
+            this.mnuPaste.Name = "mnuPaste";
+            this.mnuPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.mnuPaste.Size = new System.Drawing.Size(180, 22);
+            this.mnuPaste.Text = "&Paste";
+            this.mnuPaste.Click += new System.EventHandler(this.mnuPaste_Click);
+            // 
+            // mnuViewTextBoxes
+            // 
+            this.mnuViewTextBoxes.Name = "mnuViewTextBoxes";
+            this.mnuViewTextBoxes.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.T)));
+            this.mnuViewTextBoxes.Size = new System.Drawing.Size(229, 22);
+            this.mnuViewTextBoxes.Text = "View Text &Boxes";
+            this.mnuViewTextBoxes.Click += new System.EventHandler(this.mnuViewTextBoxes_Click);
+            // 
+            // mnuViewLabels
+            // 
+            this.mnuViewLabels.Name = "mnuViewLabels";
+            this.mnuViewLabels.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.L)));
+            this.mnuViewLabels.Size = new System.Drawing.Size(229, 22);
+            this.mnuViewLabels.Text = "View &Labels";
+            // 
+            // mnuViewImages
+            // 
+            this.mnuViewImages.Name = "mnuViewImages";
+            this.mnuViewImages.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.I)));
+            this.mnuViewImages.Size = new System.Drawing.Size(229, 22);
+            this.mnuViewImages.Text = "View &Images";
+            this.mnuViewImages.Click += new System.EventHandler(this.mnuViewImages_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(12, 148);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(200, 100);
+            this.txtBox1.TabIndex = 1;
+            this.txtBox1.Text = "This is text box 1";
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(227, 148);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(200, 100);
+            this.txtBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Text Box 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text Box 2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(457, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 284);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(642, 140);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "This is text box 1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewProj;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewFiles;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuQuit;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuUndo;
+        private System.Windows.Forms.ToolStripMenuItem mnuCut;
+        private System.Windows.Forms.ToolStripMenuItem mnuCopy;
+        private System.Windows.Forms.ToolStripMenuItem mnuPaste;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewTextBoxes;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewLabels;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewImages;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFD;
+    }
+}
+
---
+++ Menus/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Menus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void mnuQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuCut_Click(object sender, EventArgs e)
+        {
+            if (txtBox1.SelectedText != "")
+            {
+                txtBox1.Cut();
+            }
+        }
+
+        private void mnuUndo_Click(object sender, EventArgs e)
+        {
+            if (txtBox1.CanUndo == true)
+            {
+                txtBox1.Undo();
+                txtBox1.ClearUndo();
+            }
+        }
+
+        private void mnuCopy_Click(object sender, EventArgs e)
+        {
+            if (txtBox1.SelectionLength > 0)
+            {
+                txtBox1.Copy();
+            }
+        }
+
+        private void mnuPaste_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject() .GetDataPresent(DataFormats.Text) == true)
+            {
+                txtBox2.Paste();
+                Clipboard.Clear();
+            }
+        }
+
+        private void mnuViewTextBoxes_Click(object sender, EventArgs e)
+        {
+            mnuViewTextBoxes.Checked = !mnuViewTextBoxes.Checked;
+
+            if (mnuViewTextBoxes.Checked)
+            {
+                txtBox1.Visible = true;
+                txtBox2.Visible = true;
+            }
+            else
+            {
+                txtBox1.Visible = false;
+                txtBox2.Visible = false;
+            }
+        }
+
+        private void mnuViewImages_Click(object sender, EventArgs e)
+        {
+            string Chosen_File = "";
+
+            openFD.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFD.Title = "Insert an Image";
+            openFD.FileName = "";
+            openFD.Filter = "JPEG Images|*.jpg|GIF Images|*.gif|BITMAPS|*.bmp|PNG Images|*.png";
+            openFD.ShowDialog();
+
+            if (openFD.ShowDialog() != DialogResult.Cancel)
+            {
+                Chosen_File = openFD.FileName;
+                pictureBox1.Image = Image.FromFile(Chosen_File);
+            }
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            string Chosen_File = "";
+
+            openFD.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFD.Title = "Open a Text File";
+            openFD.FileName = "";
+            openFD.Filter = "Text Files|*.txt|Word Documents|*.doc";
+
+            if (openFD.ShowDialog() != DialogResult.Cancel)
+            {
+                Chosen_File = openFD.FileName;
+                richTextBox1.LoadFile(Chosen_File, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            string Saved_File = "";
+
+            saveFD.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            saveFD.Title = "Save a Text File";
+            saveFD.FileName = "";
+
+            saveFD.Filter = "Text Files|*.txt|All Files|*.*";
+
+            if (saveFD.ShowDialog() != DialogResult.Cancel)
+            {
+                Saved_File = saveFD.FileName;
+                richTextBox1.SaveFile(Saved_File, RichTextBoxStreamType.PlainText);
+            }
+        }
+    }
+}
